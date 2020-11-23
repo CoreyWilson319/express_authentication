d64d240c2dc6a56a30e35475a84b0b4151d8e48e
Your Name <you@example.com>
2020-11-23 14:34:11 -0500

    [ppConfig] Add local strategy and serializer and deserializer

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
+const session = require('express-session');
+const passport = require('./config/ppConfig');
+const flash = require('connect-flash');
+const SECRET_SESSION = process.env.SECRET_SESSION;
+// console.log(SECRET_SESSION)
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -9,6 +14,17 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+
+// secret: What we actually will be giving the user on our site as a session cookie
+// resave: Save the session even if it's modified, make this false
+// saveUninitialized: If we have a new session, we save it, therefore making that true
+
+const sessionObject = {
+  secret: SECRET_SESSION,
+  resave: false,
+  saveUninitialized: true
+}
+app.use(session(sessionObject));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -21,7 +37,7 @@ app.get('/profile', (req, res) => {
 app.use('/auth', require('./routes/auth'));
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3111;
 const server = app.listen(PORT, () => {
   console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
 });
